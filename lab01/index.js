6e984cfd639960dbc95484328787e90f7ab4cbb5
EuniceTheWeeb - <eunicethung@yahoo.com>
2024-12-27 19:39:24 +0000

    lab 3 done

--- lab01/index.js
+++ lab01/index.js
@@ -2,18 +2,19 @@ const express = require('express');
 const hbs = require('hbs');
 const app = express();
 
-app.set('view engine', 'hbs'); // <-- NOTE 2
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
 
 // ROUTES
 app.get('/', function(req,res){
     res.send("<h1>Hello from Express</h1>");
 })
 
-app.get('/hello/:name', (req,res)=>{
-    let name = req.params.name;
-    res.send("Hi, " + name);
-  })
-
+  app.get('/home', function(req, res){
+    res.render('index', {
+        companyName: "Sakura Japanese Dining"
+    }); // can leave out .hbs extension
+})
 
 
 //   MARK: Always last
